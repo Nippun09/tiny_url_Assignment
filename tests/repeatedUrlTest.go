b814555cd6f6a9ded1356ab72d81fd3bbe88133d
Nippun09 <bajaj.techie09@gmail.com>
2021-12-31 11:31:32 +0530

    add one more test for repeated url

--- tests/repeatedUrlTest.go
+++ tests/repeatedUrlTest.go
@@ -8,27 +8,26 @@ import (
 	"net/http"
 )
 
-func testShortenUrl(longUrl string) (string, bool) {
-
+func testRepeatedLongUrl(previousShortUrl, longUrl string) bool {
 	requestBody := bytes.NewBuffer([]byte(longUrl))
 	response, err := http.Post("http://localhost:8080", "application/text", requestBody)
 	if err != nil {
 		log.Fatal("failed to shorten url :", err)
-		return "", false
+		return false
 	}
 	defer response.Body.Close()
 
 	responseBody, readErr := ioutil.ReadAll(response.Body)
 	if readErr != nil {
 		log.Fatal("failed to read from response :", readErr)
-		return "", false
+		return false
 	}
 
 	shortUrl := utils.Shorten(longUrl)
 	log.Printf("short url is: %v", shortUrl)
 	log.Printf("response body is: %v", responseBody)
-	if shortUrl == string(responseBody) {
-		return shortUrl, true
+	if shortUrl == previousShortUrl {
+		return true
 	}
-	return "", false
+	return false
 }
